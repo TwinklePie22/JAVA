d0ea9386cdc3037c3326991cfc9a6238f7a6dd0b
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-07 04:16:41 +0530

    Update passwordGenerator.java

--- passwordGenerator.java
+++ passwordGenerator.java
@@ -1,50 +1,36 @@
-package JAVA_INTERNAL;
+package Lab;
 import java.util.Scanner;
 
-public class passwordGenerator
-{
-    public static void main(String[] args)
-    {
-        Scanner s=new Scanner(System.in);
-        String name;
-        String pw=new String();
-        long ph;int rno,rem,sum=0;
-        System.out.println("Enter the name : ");
-        name=s.next();
-        System.out.println("Enter the room no : ");
-        rno=s.nextInt();
-        int rno1=rno;
-        System.out.println("Enter the phone number : ");
-        ph=s.nextLong();
-        char []sym={')','!','@','#','$','%','^','&','*','('};
-        do
-        {
-            while(rno!=0)
-            {
-                rem=rno%10;
-                sum=sum+rem;
-                rno=rno/10;
-            }
-            if(sum>=10)
-            {
-                rno=sum;
-                sum=0;
-            }
-        }
-        while(rno!=0);
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the name: ");
+        String name = scanner.nextLine();
 
-        int c=rno1/100;
-        int len=name.length();
-        int pos=0;
-        for(int i=0;i<len;i++)
-        {
-            if(c==i)
-            {
-                pos=i;
-                System.out.println("Password is : ");
-            }
+        System.out.print("Enter the room number: ");
+        int roomNumber = scanner.nextInt();
+
+        System.out.print("Enter the phone number: ");
+        long phoneNumber = scanner.nextLong();
+
+        int roomDigitSum = calculateDigitSum(roomNumber);
+        int nameLength = name.length();
+        char roomDigitChar = (char) ('0' + roomDigitSum);
+
+        String password = name.charAt(0) + String.valueOf(phoneNumber % 10) + roomDigitChar + name.charAt(nameLength - 1);
+        System.out.println("Generated Password: " + password);
+    }
+
+    public static int calculateDigitSum(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
         }
-        pw=""+name.charAt(0)+(ph%10)+sum+sym[c]+name.charAt(len-1);
-        System.out.println(pw);
+        if (sum >= 10) {
+            return calculateDigitSum(sum);
+        }
+        return sum;
     }
 }
