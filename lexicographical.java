efa21a43674d7fa1cb14b2fd72b4cd703749ba27
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-06 23:56:42 +0530

    Update lexicographical.java

--- lexicographical.java
+++ lexicographical.java
@@ -1,28 +1,19 @@
-package JAVA_INTERNAL;
-import java.util.Arrays;
-import java.util.Scanner;
-public class Lexicographical {
+package Lab;
+import java.util.*;
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-
-            String[] strings = new String[5];
-
-            System.out.println("Enter 5 strings:");
-
-            for (int i = 0; i < 5; i++) {
-                strings[i] = sc.nextLine();
-            }
-
-            sc.close();
-
-            Arrays.sort(strings);
-
-            System.out.println("Strings in lexicographical order:");
-            for (String str : strings) {
-                System.out.println(str);
-            }
-        }
-    }
-
-
+public class Lexiographical {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		String []str= new String[5];
+		System.out.println("Enter 5 string values :");
+		for(int i=0; i<5 ; i++) {
+			str[i]=sc.nextLine();
+		}
+		Arrays.sort(str);
+		System.out.println("String in lexographical order :");
+		for(String s : str)
+		{
+			System.out.print(s+"  ");
+		}
+	}
+}
