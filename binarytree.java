23cfc9101fe63a5ea7a59861b6ceb7e5c354cad1
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-09-06 23:28:18 +0530

    Update binarytree.java

--- binarytree.java
+++ binarytree.java
@@ -1,31 +1,42 @@
-package JAVA_INTERNAL;
+package Lab;
+import java.util.*;
+
 class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
 
-    TreeNode(int val) {
+    public TreeNode(int val) {
         this.val = val;
+        this.left = null;
+        this.right = null;
     }
 }
-
-class binaryTreep5 {
+public class BinaryTree {
     public static void main(String[] args) {
-
-        TreeNode root = new TreeNode(10);
+        // Create the binary tree
+    	TreeNode root = new TreeNode(20);
         root.left = new TreeNode(5);
-        root.right = new TreeNode(15);
-        root.left.left = new TreeNode(2);
-        root.left.right = new TreeNode(7);
-        root.right.left = new TreeNode(12);
-        root.left.right.left = new TreeNode(8);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(9);
+        root.left.right = new TreeNode(6);
+        root.right.left = new TreeNode(15);
+        root.left.right.left = new TreeNode(21);
 
+        // Calculate the difference between the sum of odd and even level nodes
         int difference = calculateLevelDifference(root);
         System.out.println("Difference between the sum of odd level and even level nodes: " + difference);
     }
 
     private static int calculateLevelDifference(TreeNode root) {
-        return calculateLevelSum(root, 1) - calculateLevelSum(root, 0);
+        if (root == null) {
+            return 0;
+        }
+
+        int oddSum = calculateLevelSum(root, 1);  // Calculate sum at odd levels
+        int evenSum = calculateLevelSum(root, 0); // Calculate sum at even levels
+
+        return oddSum - evenSum;
     }
 
     private static int calculateLevelSum(TreeNode node, int level) {
@@ -35,10 +46,10 @@ class binaryTreep5 {
 
         // If the current level is odd, subtract the value of the node.
         // If the current level is even, add the value of the node.
-        if (level % 2 == 1) {
-            return node.val - calculateLevelSum(node.left, level + 1) - calculateLevelSum(node.right, level + 1);
-        } else {
+        if (level % 2 == 0) {
             return node.val + calculateLevelSum(node.left, level + 1) + calculateLevelSum(node.right, level + 1);
-        }
+        }else{
+            return node.val - calculateLevelSum(node.left, level + 1) - calculateLevelSum(node.right, level + 1);
+        } 
     }
 }
